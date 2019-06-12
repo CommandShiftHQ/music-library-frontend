541869d97b23c533d7f5f4f9859f87a5888448aa
Ersel Aker <erselaker@gmail.com>
2019-06-12 07:38:40 +0100

    example CRA

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      artists: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://music-library-api.herokuapp.com/artists').then(response => {
+      const artists = response.data;
+      this.setState({artists});
+    });
+  }
+
+  renderArtists = () => {
+    const { artists } = this.state;
+    if(artists.length === 0) {
+      return null;
+    }
+
+    return <div>{artists.map(artist => {
+      return <React.Fragment><span key={artist._id}>{artist.name} - {artist.genre}</span><br/><br/></React.Fragment>
+    })}</div>
+  }
+
+  render() {
+    return (
+      <div className="aksjhdjaskdh">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p class="aksjdfh">React is awesome!</p>
+          {this.renderArtists()}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
